5e9a8b1bd4e4307438c870c92bbaf4eb4fea676d
Junior-Jurado <juniorjurado2004@gmail.com>
2024-12-05 16:01:07 -0500

    first commit

---
+++ src/main/java/com/forohub/challengeAluraForoHub/ChallengeAluraForoHubApplication.java
@@ -0,0 +1,13 @@
+package com.forohub.challengeAluraForoHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeAluraForoHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeAluraForoHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/forohub/challengeAluraForoHub/ChallengeAluraForoHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forohub.challengeAluraForoHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeAluraForoHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
